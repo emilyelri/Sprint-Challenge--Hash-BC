68fb642e1a259a8720a4711f995cfc85cf29756d
Emily_Richard <richard.emily.e@gmail.com>
2020-03-13 12:53:18 -0400

    finished ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,13 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(tickets)):
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)     # inserting each ticket to the hashtable
 
-    pass
+    prev = hash_table_retrieve(hashtable, "NONE")                                   # finding start of the chain and instantiating 'prev' variable
+    
+    for i in range(length):                                                         # for each value in the length of the route
+        route[i] = prev                                                             # it's setting that bucket to the prev item's 'destination' (current location)
+        prev = hash_table_retrieve(hashtable, route[i])                             # and then setting the prev to current value's 'source' (key)
+
+    return route[:-1]
